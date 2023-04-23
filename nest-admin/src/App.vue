da9d064bf01f568a91853070188c1b6cf930cb16
fuyunlong <fuyunlong@delonix.group>
2023-04-23 12:01:23 +0800

    first commit

---
+++ nest-admin/src/App.vue
@@ -0,0 +1,13 @@
+
+<template>
+  <Login msg="Vite + Vue" />
+</template>
+
+
+<script setup lang="ts">
+import Login from './components/Login.vue'
+</script>
+
+
+<style scoped>
+</style>
---
+++ nest-admin/src/components/Login.vue
@@ -0,0 +1,20 @@
+<template>
+  <p>
+    Install
+    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
+    in your IDE for a better DX
+  </p>
+</template>
+
+
+<script setup lang="ts">
+
+
+
+</script>
+
+
+
+<style scoped>
+
+</style>
